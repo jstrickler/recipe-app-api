89b28e7332b8ab8168d4dc760f739baa170a5254
jstrickler <jstrickler@gmail.com>
2020-09-13 13:15:51 -0400

    Added endpoint for creating and authenticating users

---
+++ app/user/serializers.py
@@ -0,0 +1,51 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = 'email', 'password', 'name'
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'min_length': 5,
+            }
+            # cpi;d add extra args fpr password or name
+        }
+
+    def create(self, validated_data):
+        """Create a new user with encryped password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')  # passed from serializer
+        password = attrs.get('password')  # passed from serializer
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,  # param for authenticate
+            password=password,
+        )
+
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
